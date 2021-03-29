9f64f72b1e74c6260878b859ef617ee88f78da22
pdavis213 <78828994+pdavis213@users.noreply.github.com>
2021-03-29 14:23:52 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let image=document.getElementById('img')
+let hide=document.getElementById('hide')
+let show=document.getElementById('show')
+
+hide.onclick=function(){
+    image.style.display='none'
+}
+show.onclick=function(){
+    image.style.display='block'
+}
